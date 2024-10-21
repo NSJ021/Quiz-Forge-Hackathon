d88a736d7af332c91a0eaf93c6f12acc716f3c14
Richard Hallam <richardhallam25@gmail.com>
2024-10-21 13:07:21 +0000

    Added meta tags, Added buttons and question area

---
+++ assets/scripts/script.js
@@ -0,0 +1,6 @@
+//button hide script go through an array of buttons and set to visible as you add content hidden by default
+
+
+//initalise buttons: hide buttons 
+
+//load question: get data from json and populate buttons, set populated buttons to visible.
