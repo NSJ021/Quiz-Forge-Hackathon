6acb017738fac26895c589fa183d216a2491225d
Richard Hallam <richardhallam25@gmail.com>
2024-10-21 13:37:04 +0000

    added rough script layout

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,26 @@
-//button hide script go through an array of buttons and set to visible as you add content hidden by default
+
+/**
+ * initialisation
+ */
+
+//questionButtonInitialise() go through an array of buttons and set to visible as you add content hidden by default. Adds event listener
+
+//loadQuestion() get data from json and populate buttons, set populated buttons to visible.
+
+//initialiseNavButtons() - add event listener.
 
 
-//initalise buttons: hide buttons 
+/**
+ * player board function
+ */
 
-//load question: get data from json and populate buttons, set populated buttons to visible.
+//movePlayer(){}  moves player
+//updateScoreCounter 
+//checkAnswer
+//
+
+/**
+ * Nav buttons 
+ */
+
+//displayModal() based on button clicked displays a specific modal.
